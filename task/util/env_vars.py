1cca3b7c3fe015cbf0c8f1a3b58dd267da5b786b
Andres M. Prieto <andresprieto2703@gmail.com>
2024-05-01 03:13:32 -0500

    Tracking env vars

---
+++ task/util/env_vars.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from os.path import join, dirname
+
+
+project_path = Path(dirname(__file__))
+env_file = join(project_path.parent.absolute(), ".env")
+
+load_dotenv(env_file)
+
+config = {
+    "image_dir": os.environ.get("IMAGE_DIR"),
+    "save_dir": os.environ.get("SAVE_DIR"),
+
+    "web_site": os.environ.get("WEB_SITE"),
+
+    "verbose": os.environ.get("VERBOSE") == "1",
+    "autoclear": os.environ.get("AUTOCLEAR") == "1",
+}
