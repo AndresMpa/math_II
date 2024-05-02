5abf8953b090e4f13a53455f295c8b82c4ff0310
Andres M. Prieto <andresprieto2703@gmail.com>
2024-05-02 06:02:31 -0500

    Better management for files

--- task/vector_and_matrices/main.py
+++ task/vector_and_matrices/main.py
@@ -4,6 +4,12 @@ from test_sets.main import select_stage
 from util.dirs import clear_dir, get_current_path
 
 if __name__ == '__main__':
+    if (config["autoclear"]):
+        clear_dir(get_current_path(config['mtx_dir']))
+        clear_dir(get_current_path(config['image_dir']))
+        clear_dir(get_current_path(config['save_image_dir']))
+        clear_dir(get_current_path(config['save_matrix_dir']))
+
     timestamp = str(time.time())
 
     test = select_stage(config["mode"])
@@ -12,7 +18,3 @@ if __name__ == '__main__':
         raise Exception("There is an error on your .env file")
 
     test(timestamp)
-
-    if (config["autoclear"]):
-        clear_dir(get_current_path(config['image_dir']))
-        clear_dir(get_current_path(config['mtx_dir']))
